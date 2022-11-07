7312d11b47a6a4e354be5efa97d31878e7f72610
Elijah Piper <35882341+Elijah-Piper@users.noreply.github.com>
2022-11-07 09:10:39 -0700

    Created dist and src folders along with a single file for testing type annotations

---
+++ dist/typeAnnotationTest.js
@@ -0,0 +1,6 @@
+"use strict";
+let num = 20;
+while (num < 40) {
+    num += 10;
+    console.log(num);
+}
---
+++ src/typeAnnotationTest.ts
@@ -0,0 +1,7 @@
+let num: number = 20;
+
+
+while (num < 40) {
+    num += 10;
+    console.log(num);
+}
